648692297f761524ab67738965af8b527305cfd8
Kang <kangjianhu@conflux-chain.org>
2023-10-20 11:56:07 +0800

    show token symbol

--- src/tx-tracer/AddrName.tsx
+++ src/tx-tracer/AddrName.tsx
@@ -10,6 +10,7 @@ export const AddrName=({addr}:{addr:string})=>{
     const [v,setV] = useState({
         'ContractName': '', Implementation:'', loading: true, error: false as boolean|string
     })
+    const [tokenInfo, setTokenInfo] = useState({symbol: ''})
     useEffect(()=>{
         fetchWithCache(`${rpcHolder.api}/api?module=contract&action=getsourcecode&address=${addr}`).then(res=>{
             const first = (res.result || [])[0];
@@ -22,11 +23,14 @@ export const AddrName=({addr}:{addr:string})=>{
         }).catch(e=>{
             setV({...v, error: `api error: ${e}`})
         })
+        fetchWithCache(`${rpcHolder.api}/api?module=token&action=tokeninfo&contractaddress=${addr}`).then(res=>{
+            res.result && setTokenInfo(res.result[0])
+        })
     }, [addr])
     return (
         <Space direction={'vertical'} size={'small'}>
             <Addr addr={addr} short={true}/>
-            {v?.ContractName?.split(':')[1] || v?.ContractName}
+            <span>{v?.ContractName?.split(':')[1] || v?.ContractName}{tokenInfo?.symbol ? `(${tokenInfo.symbol})` : ''}</span>
             {v?.Implementation &&
                 <AddrName addr={v.Implementation}/>
             }
