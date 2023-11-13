6d69378a968de64b35b46cf0e7de4254baf0554d
Kang <kangjianhu@conflux-chain.org>
2023-11-13 17:47:45 +0800

    polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,65 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
+    }
+  },
+  server: {
+    proxy: {
+      '/nft-house': {
+        target: 'http://127.0.0.1:8008',
+        changeOrigin: true,
+        configure: (proxy, _options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log(
+                "Sending Request:",
+                req.method,
+                req.url,
+                " => TO THE TARGET =>  ",
+                proxyReq.method,
+                proxyReq.protocol,
+                proxyReq.host,
+                proxyReq.path,
+                // JSON.stringify(proxyReq.getHeaders()),
+            );
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+                "Received Response from the Target:",
+                proxyRes.statusCode,
+                req.url,
+                JSON.stringify(proxyRes.headers),
+            );
+          });
+        },
+      },
+      '/proxy': {
+        target: "https://evmtestnet.confluxrpc.com",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/api': {
+        target: "https://evmapi-testnet.confluxscan.io",
+        changeOrigin: true,
+      }
+    },
+  },
 })
